d248550ff03e9dafd64e93a2371fef4735607f3d
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-14 10:28:25 -0300

    feat: add variable examples

---
+++ variable.rb
@@ -0,0 +1,7 @@
+# variable names must use lowercase letters and underscores (snake_case)
+$global_name = "Luiz"
+@instance_name = "Fernando"
+local_name = "Meier"
+
+# constants must start with a capital letter and use CamelCase
+ConstantName = "this value can't be changed"
