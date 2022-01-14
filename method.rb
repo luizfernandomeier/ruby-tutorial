12ef0270f9996db0adee2d45a83bda02630687aa
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-14 10:28:13 -0300

    feat: add method examples

--- method.rb
+++ method.rb
@@ -1,4 +1,6 @@
+# method and argument names must use lowercase letters and underscores (snake_case)
 
+# method with default argument
 def show_welcome_message(name, year=2010)
   puts("Welcome to #{year}, #{name}!")
 end
@@ -7,3 +9,10 @@ end
 show_welcome_message("Luiz", "2020")
 show_welcome_message "Fernando", 2019
 show_welcome_message "Meier"
+
+# method with implicit return
+def sum(a, b)
+  a + b
+end
+
+puts "Sum result: #{sum(1, 2)}"
