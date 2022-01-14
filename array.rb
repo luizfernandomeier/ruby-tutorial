3dace7182b4734c823c124088e907f878764bd6c
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-14 13:39:36 -0300

    feat: add hash examples

--- array.rb
+++ array.rb
@@ -41,3 +41,12 @@ repeated_hash = {
 }
 puts repeated_hash
 puts repeated_hash['b']
+
+# searching for a non existent key just returns a nil value
+puts repeated_hash['non_existent_key']
+
+# create a new hash map with a default value of zero
+default_hash = Hash.new(0)
+puts default_hash['a'] # 0
+default_hash['a'] = 1
+puts default_hash['a'] # 1
