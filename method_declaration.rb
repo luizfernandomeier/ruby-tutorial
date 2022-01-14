36d88a91f12481cbdbcbce6baea3b4d3757cf7a6
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-14 09:17:12 -0300

    feat: add method declaration examples

---
+++ method_declaration.rb
@@ -0,0 +1,8 @@
+
+def show_welcome_message(name, year=2010)
+  puts("Welcome to #{year}, #{name}!")
+end
+
+show_welcome_message("Luiz", "2020")
+show_welcome_message "Fernando", 2019
+show_welcome_message "Meier"
