fa467b7f533405a36f6a72bf8043935f9410ef5d
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-14 17:16:23 -0300

    feat: add input examples

---
+++ input.rb
@@ -0,0 +1,4 @@
+# gets the next line from the standard input
+line = gets # couldn't get vscode to handle this, but running the program in the terminal works fine
+puts "Data: #{line}"
+puts "Magic data: #$_" # this may be depracted in the future
