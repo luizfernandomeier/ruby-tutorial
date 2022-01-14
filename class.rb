1f2d6a42479ee4959c8fc345653bf632476c9943
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-14 17:43:19 -0300

    feat: override to_s method

--- class.rb
+++ class.rb
@@ -1,11 +1,18 @@
+# there are no closed classes in ruby, including the built-in classes
+
 class Song
   def initialize(name, artist, duration_seconds)
     @name = name
     @artist = artist
     @duration_seconds = duration_seconds
   end
+
+  def to_s
+    "Song: #@name--#@artist (#@duration_seconds seconds)"
+  end
 end
 
 bits_of_parrots = Song.new("Bits of Parrots", "Enemies", 244)
 
-puts bits_of_parrots.inspect
+puts bits_of_parrots.inspect # prints the id and all instance variables
+puts bits_of_parrots.to_s # prints just the id if not overridden
