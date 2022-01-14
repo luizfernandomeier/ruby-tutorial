e3e47b76440ed7847afa45f58b47fc2eb8518c57
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-14 09:22:09 -0300

    feat: add parentheses recommendation

--- method_declaration.rb
+++ method_declaration.rb
@@ -3,6 +3,7 @@ def show_welcome_message(name, year=2010)
   puts("Welcome to #{year}, #{name}!")
 end
 
+# because of precedence rules, it is recommended to use parentheses in all but the simplest cases
 show_welcome_message("Luiz", "2020")
 show_welcome_message "Fernando", 2019
 show_welcome_message "Meier"
