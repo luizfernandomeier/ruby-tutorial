5c9bd599572e9158d33e4b79e7a2eab5f1a3ef87
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-15 16:29:15 -0300

    feat: variable references examples

--- variable.rb
+++ variable.rb
@@ -10,3 +10,35 @@ ConstantName = "this value can't be changed"
 nil_a = nil
 nil_b = nil
 puts nil_a == nil_b # true
+
+# variable references
+person1 = "Tim"
+person2 = person1 # just copies the reference
+puts "person1: #{person1}, person2: #{person2}"
+
+person1[0] = "J"
+puts "person1: #{person1}, person2: #{person2}"
+
+person2 = "John"
+puts "person1: #{person1}, person2: #{person2}"
+
+person2 = person1.dup # duplicates the object with a different reference
+puts "person1: #{person1}, person2: #{person2}"
+
+person1[0] = "T"
+puts "person1: #{person1}, person2: #{person2}"
+
+person2 = person1
+puts "person1: #{person1}, person2: #{person2}"
+
+person1.freeze
+begin
+  person1[0] = "J"
+rescue
+  puts "Can't modify frozen string"
+end
+begin
+  person2[0] = "J"
+rescue
+  puts "Can't modify frozen string"
+end
