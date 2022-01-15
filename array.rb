df9655f215278e6a0b141a74e55ab9ba327ec68b
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-15 19:32:26 -0300

    refactor: split hash in another file

--- array.rb
+++ array.rb
@@ -41,34 +41,6 @@ puts multitype_array[multitype_array.length-1]
 shortcut_array = %w{one two three}
 puts shortcut_array
 
-# hash map (dictionary) - the last comma is optional
-country_continent = {
-  'Argentina' => 'South America',
-  'Brazil' => 'South America',
-  'Germany' => 'Europe',
-  'Japan' => 'Asia',
-}
-puts country_continent
-puts country_continent['Japan']
-
-# if you define a hash map with two identical keys, the last one is considered
-repeated_hash = {
-  'a' => 'b',
-  'a' => 'c',
-  'b' => 'd',
-}
-puts repeated_hash
-puts repeated_hash['b']
-
-# searching for a non existent key just returns a nil value
-puts repeated_hash['non_existent_key']
-
-# create a new hash map with a default value of zero
-default_hash = Hash.new(0)
-puts default_hash['a'] # 0
-default_hash['a'] = 1
-puts default_hash['a'] # 1
-
 # another ways of starting an array
 array1 = Array.new
 puts "#{array1.class}[#{array1.length}]"
---
+++ hash.rb
@@ -0,0 +1,27 @@
+# hash map (dictionary) - the last comma is optional
+country_continent = {
+  'Argentina' => 'South America',
+  'Brazil' => 'South America',
+  'Germany' => 'Europe',
+  'Japan' => 'Asia',
+}
+puts country_continent
+puts country_continent['Japan']
+
+# if you define a hash map with two identical keys, the last one is considered
+repeated_hash = {
+  'a' => 'b',
+  'a' => 'c',
+  'b' => 'd',
+}
+puts repeated_hash
+puts repeated_hash['b']
+
+# searching for a non existent key just returns a nil value
+puts repeated_hash['non_existent_key']
+
+# create a new hash map with a default value of zero
+default_hash = Hash.new(0)
+puts default_hash['a'] # 0
+default_hash['a'] = 1
+puts default_hash['a'] # 1
