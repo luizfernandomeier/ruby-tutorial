a63a3f0ce3fbb9e8bb01eced078b007dfc270158
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-15 01:41:42 -0300

    feat: add number examples

---
+++ number.rb
@@ -0,0 +1,6 @@
+puts float_number = 3.14159
+puts float_number.class # Float
+printf("%0.100f\n", float_number) # shows the float imprecision
+
+puts float_number.to_i # 3
+puts float_number.to_i.class # Integer
