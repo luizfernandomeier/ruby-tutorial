504c97ecdc624aabeaed58d06578201f992d9465
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-15 20:52:10 -0300

    feat: todo outdated behavior

---
+++ outdated_behavior.rb
@@ -0,0 +1,8 @@
+# TODO: this example is not working (page 76)
+# the b variable is being replaced by the block parameter
+# maybe it is outdated
+a = [1, 2]
+b = 'cat'
+a.each {|b| c = b * a[1] }
+puts "a: #{a}, b: #{b}"
+puts defined?(c) == nil
