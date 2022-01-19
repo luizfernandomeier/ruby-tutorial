2f2b70ea7b3fbed396d9d0bf11918b2caed82cff
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-19 09:27:31 -0300

    feat: add range capable classes examples

--- range.rb
+++ range.rb
@@ -17,3 +17,45 @@ puts digits.min # 0
 puts digits.max # 9
 print digits.reject {|i| i % 2 == 0} # 1,3,5,7,9 - retuns an array
 puts
+
+# range capable class
+class VU
+  attr :volume
+  # mixin that includes the Comparable method like 'between'
+  include Comparable
+
+  def initialize(volume) # 0..9
+    @volume = volume
+  end
+
+  def inspect
+    "Volume: #{@volume}"
+  end
+
+  # support for ranges - both methods <=> and succ are necessary
+  def <=> (other) # spaceship operator / comparison method
+    self.volume <=> other.volume
+  end
+
+  # called for each item on the iteration
+  def succ
+     # just limits the range iteration, doesn't work with 'new'
+    raise(IndexError, "Volume too high") if @volume >= 9
+    VU.new(@volume.succ)
+  end
+end
+
+vu = VU.new(100)
+puts vu.inspect
+print (VU.new(0)..VU.new(9)).to_a
+puts
+
+# error raised in succ - it only checks when the range is iterated
+begin
+  (VU.new(0)..VU.new(10)).to_a
+rescue
+  puts "Rescued error: #{$!}"
+end
+
+# between is a method of Comparable module
+puts VU.new(2).between?(VU.new(1), VU.new(3)) # true
