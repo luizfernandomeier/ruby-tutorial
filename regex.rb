7a48635facb9d3cdadeaecce75af9397d794fa15
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-19 11:48:19 -0300

    feat: add regex examples

--- regex.rb
+++ regex.rb
@@ -1,15 +1,27 @@
-text = "a123b456"
+text = "ba123b456"
 regex = /[1-9]+/
 
 # in ruby, even the regex is an object
 puts /[1-9]+/.match(text) # 123
 
 # print all matches - it works differently if the regex has groups
-puts text.scan(regex)
+print text.scan(regex)
+puts
 
 # special regex match operator
 if text =~ regex
   puts "match: #{text}"
+  puts "match position: #{text =~ regex}"
+  puts "preceding match text: #{$`}"
+  puts "match text: #{$&}"
+  puts "after the match text: #{$'}"
+  puts "match decorated: #{$`}<<#{$&}>>#{$'}"
+  puts "match data object: #{$~.class}"
+end
+
+# negative match
+if text !~ /7-9/
+  puts "didn't match"
 end
 
 puts "match: #{text}" if text =~ regex
@@ -19,3 +31,14 @@ puts text.sub(regex, 'NEW TEXT')
 
 # replace all substrings matched by regex
 puts text.gsub(regex, 'NEW TEXT')
+
+# this function can use blocks
+puts "Luiz Fernando".sub(/\w+/) { |match| match.upcase } # LUIZ Fernando
+
+# group variables
+puts /\d\d[:]\d\d/.match("12:50")
+puts "Hour: #{$1}, Minutes: #{$2}" # Hour: , Minutes: 
+puts /(\d\d)[:](\d\d)/.match("12:50")
+puts "Hour: #{$1}, Minutes: #{$2}" # Hour: 12, Minutes: 50
+puts /((\d\d)[:](\d\d))/.match("12:50")
+puts "Time: #{$1}, Hour: #{$2}, Minutes: #{$3}" # Time: 12:50, Hour: 12, Minutes: 50
