36b6a5e5d39d1d9f26dbfe6c0de5729717627cfc
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-21 18:46:30 -0300

    feat: add file handling examples

--- file.rb
+++ file.rb
@@ -1,3 +1,7 @@
 f = File.open("sample_file.txt")
 f.each {|line| puts line }
 f.close
+
+# TODO: I didn't get this one right - why I don't have to close the file here?
+# the block is being passed on to `grep`, no to `open`.
+f = File.open("sample_file.txt").grep(/^This/) {|line| puts line }
