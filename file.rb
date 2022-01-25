a29798fdbd205ba46102659db03bbfc4e82ecb17
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-25 15:56:23 -0300

    feat: add file examples

--- file.rb
+++ file.rb
@@ -1,7 +1,31 @@
 f = File.open("sample_file.txt")
 f.each {|line| puts line }
 f.close
+puts '--------'
 
-# TODO: I didn't get this one right - why I don't have to close the file here?
-# the block is being passed on to `grep`, no to `open`.
+# TODO: try to simulate an error on a file that is not closed properly, then see if this one closes it:
 f = File.open("sample_file.txt").grep(/^This/) {|line| puts line }
+puts '--------'
+
+f = File.new("sample_file.txt", "r") # "r" is the default mode (reading); w is writing; r+ is reading and writing; a is appending
+f.each_line { |line| puts line }
+f.seek(0) # go back to the beginning of the file
+f.each_byte { |ch| print "."; putc ch }
+f.close
+puts '--------'
+
+# create a new file - IO
+f = File.new("dummy_file.txt", "a") # append also creates the file if it doesn't exist
+f.puts "PUTS"
+f.print "PRINT\n"
+f << "<<\n"
+f << "\001\002\007" # binary data
+f.close
+
+File.open("dummy_file.txt") do |f|
+  f.each {|line| puts line }
+end
+puts '--------'
+
+# delete file
+File.delete("dummy_file.txt")
