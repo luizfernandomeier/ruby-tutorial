39fccd754936e492c49c9b02eb1dd03ed616a583
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-25 15:55:49 -0300

    feat: add ruby application arguments example

---
+++ change_gets_behavior.rb
@@ -0,0 +1,5 @@
+# if you run this program without arguments, the `gets` will get whatever you type
+# if you run it with a file as an argument, it will use it as the source to `gets`
+while line = gets
+  puts line
+end
